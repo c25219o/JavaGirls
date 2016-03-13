1fddb5658931ae14fa8084b7a395dee46fca6318
pistachio <c25219o@gmail.com>
2016-03-13 15:17:27 +0900

    progress

---
+++ src/day01_mar13/C04_Condition.java
@@ -0,0 +1,25 @@
+package day01_mar13;
+
+public class C04_Condition {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b= ++a;
+
+
+		switch (a) {
+		case 1 :
+			System.out.println("1");
+			break;
+
+		case 10 :
+			System.out.println("2");
+			break;
+
+		case 11 :
+			System.out.println("3");
+			break;
+		}
+	}
+
+}
---
+++ src/day01_mar13/C05_Fork.java
@@ -0,0 +1,13 @@
+package day01_mar13;
+
+public class C05_Fork {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+
+		String str = x > y ? "JJUG" : "Java-jo";
+		System.out.println(str);
+	}
+
+}
